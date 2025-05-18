e4a8777af96c95c970a6d3a0c29bdb43f49fb047
ismaeinz <hire_me_ismaein@hotmail.com>
2025-05-18 07:23:39 +0300

    finish responed

---
+++ AndroidProjects/android_basic/Movies/app/src/main/java/com/example/movies/app/data/local/remote/MovieApi.kt
@@ -0,0 +1,20 @@
+package com.example.movies.app.data.local.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MovieApi {
+    @GET("movie/{category}")
+    suspend fun getMoviesList(
+        @Path("category") category: String,
+        @Path("page") page: Int,
+        @Path("api_key") apikey: String = API_KEY,
+
+        )
+
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+        const val API_KEY = "aee2e249581b1ef7ed2cb17f5629ef39"
+    }
+}
